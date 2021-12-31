5d72fcff31adc124cad5d5a309bdfead4267eaa0
VongolaTeong <0522teongkarchun@gmail.com>
2021-12-31 13:29:07 +0800

    add notification

--- app/src/main/java/com/example/timer/AlarmReceiver.kt
+++ app/src/main/java/com/example/timer/AlarmReceiver.kt
@@ -1,13 +1,24 @@
 package com.example.timer
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Context.NOTIFICATION_SERVICE
 import android.content.Intent
+import android.os.Build
 import android.util.Log
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+
 
 //receiver to receive alarms
 class AlarmReceiver : BroadcastReceiver() {
-    override fun onReceive(context: Context?, intent: Intent) {
+    private val notificationID = 100
+    private val channelID = "1000"
+
+    override fun onReceive(context: Context, intent: Intent) {
         if (Intent.ACTION_BOOT_COMPLETED == intent.action) {
             //when rebooted, reset all alarms
             //TODO
@@ -15,6 +26,43 @@ class AlarmReceiver : BroadcastReceiver() {
             //send alarm notification
             //TODO
             Log.i("alarm", "alarm sent")
+            createChannel(context)
+            createNotification(context)
+        }
+    }
+
+    //create notification channel
+    private fun createChannel(context: Context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name:String = context.getString(R.string.channel_name)
+            val descriptionText = context.getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val mChannel = NotificationChannel(channelID, name, importance)
+            mChannel.description = descriptionText
+
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            val notificationManager = context.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(mChannel)
+        }
+    }
+
+    private fun createNotification(context: Context) {
+        // Create an explicit intent for an Activity in your app
+        val intent = Intent(context, AlarmReceiver::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
+
+        val builder = NotificationCompat.Builder(context, channelID)
+            .setSmallIcon(R.drawable.ic_alarm_add)
+            .setContentTitle(context.getString(R.string.channel_name))
+            .setContentText(context.getString(R.string.alarm))
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+
+        with(NotificationManagerCompat.from(context)) {
+            notify(notificationID, builder.build())
         }
     }
 }
